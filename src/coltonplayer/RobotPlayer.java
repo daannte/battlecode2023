3232cd817b876445fd88804dd6a1b5ea46328553
Colton Gowans <coltongowans@gmail.com>
2023-01-12 14:45:55 -0700

    Deleted old commented code

--- src/coltonplayer/RobotPlayer.java
+++ src/coltonplayer/RobotPlayer.java
@@ -122,27 +122,7 @@ public strictfp class RobotPlayer {
             rc.writeSharedArray(indicator+2, me.y);
             rc.writeSharedArray(0, indicator+2);
         }
-        // Pick a direction to build in.
-        Direction dir = directions[rng.nextInt(directions.length)];
-        // MapLocation newLoc = rc.getLocation().add(dir);
-//        if (rc.canBuildAnchor(Anchor.STANDARD)) {
-//            // If we can build an anchor do it!
-//            rc.buildAnchor(Anchor.STANDARD);
-//            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
-//        }
-//        if (rng.nextBoolean()) {
-//            // Let's try to build a carrier.
-//            rc.setIndicatorString("Trying to build a carrier");
-//            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
-//                rc.buildRobot(RobotType.CARRIER, newLoc);
-//            }
-//        } else {
-//            // Let's try to build a launcher.
-//            rc.setIndicatorString("Trying to build a launcher");
-//            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
-//                rc.buildRobot(RobotType.LAUNCHER, newLoc);
-//            }
-//        }
+
         // Pick a direction to build in.
         for (Direction direction : directions) {
             MapLocation spawnLocation = rc.getLocation().add(direction);
