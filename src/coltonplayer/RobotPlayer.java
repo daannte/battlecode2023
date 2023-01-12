0f02f2fb5f000fa5bf9e3b2f3311ce094fb0c789
Colton Gowans <coltongowans@gmail.com>
2023-01-12 14:22:53 -0700

    HQ tries multiple spawn locations for dudes

--- src/coltonplayer/RobotPlayer.java
+++ src/coltonplayer/RobotPlayer.java
@@ -124,23 +124,33 @@ public strictfp class RobotPlayer {
         }
         // Pick a direction to build in.
         Direction dir = directions[rng.nextInt(directions.length)];
-        MapLocation newLoc = rc.getLocation().add(dir);
+        // MapLocation newLoc = rc.getLocation().add(dir);
 //        if (rc.canBuildAnchor(Anchor.STANDARD)) {
 //            // If we can build an anchor do it!
 //            rc.buildAnchor(Anchor.STANDARD);
 //            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
 //        }
-        if (rng.nextBoolean()) {
-            // Let's try to build a carrier.
-            rc.setIndicatorString("Trying to build a carrier");
-            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
-                rc.buildRobot(RobotType.CARRIER, newLoc);
+//        if (rng.nextBoolean()) {
+//            // Let's try to build a carrier.
+//            rc.setIndicatorString("Trying to build a carrier");
+//            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+//                rc.buildRobot(RobotType.CARRIER, newLoc);
+//            }
+//        } else {
+//            // Let's try to build a launcher.
+//            rc.setIndicatorString("Trying to build a launcher");
+//            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+//                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+//            }
+//        }
+        // Pick a direction to build in.
+        for (Direction direction : directions) {
+            MapLocation spawnLocation = rc.getLocation().add(direction);
+            if (rc.canBuildRobot(RobotType.CARRIER, spawnLocation)) {
+                rc.buildRobot(RobotType.CARRIER, spawnLocation);
             }
-        } else {
-            // Let's try to build a launcher.
-            rc.setIndicatorString("Trying to build a launcher");
-            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
-                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            if(rc.canBuildRobot(RobotType.LAUNCHER, spawnLocation)){
+                rc.buildRobot(RobotType.LAUNCHER, spawnLocation);
             }
         }
     }
