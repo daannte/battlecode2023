79b9dc488bc4a90cf79f63e77e5114b734bafb56
CarterTI84 <ctam587@gmail.com>
2023-01-12 22:04:59 -0700

    Added unit counters

--- src/carterplayer/RobotPlayer.java
+++ src/carterplayer/RobotPlayer.java
@@ -325,7 +325,7 @@ public strictfp class RobotPlayer {
         // Also try to move randomly.
 
         if (rc.canMove(dir)) {
-            rc.move(dir);//a
+            rc.move(dir);//aa
         }
         else {
             dir = directions[rng.nextInt(directions.length)];
